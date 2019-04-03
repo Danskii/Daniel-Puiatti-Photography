aa61903956bf11be971dab2599eb84546377b115
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-04-03 12:10:32 -0400

    Updates

---
+++ precache-manifest.ff989f69022d590174576ebba4103f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1512730f1f9e852580f9e40440d4cdcc",
+    "url": "/Daniel-Puiatti-Photography/static/media/instagram.1512730f.png"
+  },
+  {
+    "revision": "d37f92cd5be8269b64f7d9b1d6908982",
+    "url": "/Daniel-Puiatti-Photography/static/media/github.d37f92cd.png"
+  },
+  {
+    "revision": "affef39cb198fc2ac457",
+    "url": "/Daniel-Puiatti-Photography/static/js/runtime~main.ffd4b2d6.js"
+  },
+  {
+    "revision": "2698aefd6ac133b9a4aa",
+    "url": "/Daniel-Puiatti-Photography/static/js/main.99ac73d1.chunk.js"
+  },
+  {
+    "revision": "31a18071e7dff3aba1c5",
+    "url": "/Daniel-Puiatti-Photography/static/js/2.0568c3cd.chunk.js"
+  },
+  {
+    "revision": "2698aefd6ac133b9a4aa",
+    "url": "/Daniel-Puiatti-Photography/static/css/main.8d057b1e.chunk.css"
+  },
+  {
+    "revision": "60f2b19fcb82fe6c9c5b3ba55e2f3135",
+    "url": "/Daniel-Puiatti-Photography/index.html"
+  }
+];
