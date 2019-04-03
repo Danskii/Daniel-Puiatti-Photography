8259ad1a9dae7970dccf9dc5756cf53f84278740
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-04-03 09:02:25 -0400

    changes

--- development/src/footer.js
+++ development/src/footer.js
@@ -10,13 +10,13 @@ class Footer extends React.Component {
 <div>
 
 
-<div className="FooterWrapper w-100  fixed bottom-0 bg-black pa3" >
-<div className="fl w-75 white Athelas f4 lh-copy">Daniel Puiatti Photography</div>
-<div className="fl w-25" >
-<div className="fl w-25 white tc" >Twitter</div>
-<div className="fl w-25 white tc" >Instagram</div>
-<div className="fl w-25 white tc" >Email</div>
+<div className="FooterWrapper w-100  fixed bottom-0 bg-black pa2" >
+<div className="fl w-60 white Athelas f4 lh-copy">Daniel Puiatti Photography</div>
+<div className="fl w-40 fr" >
+<div className="fl w-25 white tc" >Tw</div>
+<div className="fl w-25 white tc" >Ig</div>
 <div className="fl w-25 white tc" >Git</div>
+<div className="fl w-25 white tc" >Em</div>
 </div>
 
 </div>
