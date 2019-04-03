f9f8a73410557e8f49e2844b621d833d1242346e
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-04-03 12:11:14 -0400

    changes

--- development/src/footer.js
+++ development/src/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'tachyons';
+
 
 class Footer extends React.Component {
 
@@ -10,27 +10,14 @@ class Footer extends React.Component {
 <div>
 
 
-<div className="FooterWrapper flex  fixed bottom-0" >
-<div className="fl w-50 white Athelas f4 lh-copy ">Daniel Puiatti Photography</div>
-<div className="fl w-50 fr" >
-<div className="fl w-25 white fab fa-twitter ma2">
-<img  className="" src={require("../src/icons/twitter.png")} />
-</div>
-<div className="fl w-25 white fab fa-twitter ma2">
-<img  className=""   src={require("../src/icons/instagram.png")} />
-</div>
-<div className="fl w-25 white fab fa-twitter ma2">
-<img  className=""   src={require("../src/icons/linkedin.png")} />
-</div>
-<div className="fl w-25 white fab fa-twitter ma2">
-<img   className=""  src={require("../src/icons/github.png")} />
+<div className="FooterWrapper">
+<div className="SocialIconWrapper">
+<img className="SocialIcon"  src={require("../src/icons/twitter.png")} onClick={()=> window.open("http://www.twitter.com/dpuiatti", "_blank")} />
+<img className="SocialIcon"  src={require("../src/icons/instagram.png")} onClick={()=> window.open("http://www.instagram.com/ddaannskii", "_blank")} />
+<img className="SocialIcon"  src={require("../src/icons/linkedin.png")} onClick={()=> window.open("https://www.linkedin.com/in/daniel-puiatti-15688312/", "_blank")}/>
+<img className="SocialIcon"  src={require("../src/icons/github.png")} onClick={()=> window.open("https://github.com/Danskii", "_blank")}/>
 </div>
 </div>
-
-</div>
-
-
-
 </div>
         )
 
